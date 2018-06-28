f6742aac6d3321dc4b6d84cb4c524cfef7c41111
Karthik <karthik19894@gmail.com>
2018-06-28 23:38:33 +0530

    Added Create and Update Routes for Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,7 +8,7 @@ const Profile=require('../../models/Profile');
 
 router.get('/test',(req,res)=>res.json({msg:'Profile works'}));
 
-//Profile ROute-Protected
+//Profile Route-Protected
 router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
     const errors={};
     Profile.findOne({user:req.user.id})
@@ -23,5 +23,47 @@ router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
 });
 
 
+//Create profile route
+
+router.post('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    //Get Fields
+    const profileFields={};
+    profileFields.user=req.user;
+
+    if(req.body.handle)profileFields.handle=req.body.handle;
+    if(req.body.company)profileFields.company=req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    if(typeof req.body.skills!=undefined){
+        profileFields.skills=req.body.skills.split(',');
+    }
+
+    //Social
+
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({user:req.user.id})
+    .then(profile=>{
+        if(profile){
+            Profile.findOneAndUpdate({user:req.user.id},{$set:profileFields},{new:true})
+            .then(profile=>res.json(profile));
+        }
+        else{
+            //Create
+        }
+    })
+
+
+})
 
 module.exports=router;
