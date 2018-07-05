f78c6ab054e185a588ffcde44ec0d80f206550e9
Karthik <karthik19894@gmail.com>
2018-07-06 00:26:00 +0530

    Completed Registration Service and Frontend

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,47 +13,45 @@ router.get('/test',(req,res)=>res.json({msg:'Users works'}));
 
 
 //Register route
-router.post('/register',(req,res)=>{
-    const {errors,isValid}=validateRegisterInput(req.body);
-
-    //Check Validation
-    if(!isValid){
-        return res.status(400).json(errors);
+router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+  
+    // Check Validation
+    if (!isValid) {
+      return res.status(400).json(errors);
     }
-
-    
-    User.findOne({email:req.body.email})
-    .then(user=>{
-        if(user){
-            errors.email='Email already exists';
-            return res.status(400).json(erros);
-        }else{
-            const avatar=gravatar.url(req.body.email,{
-                s:'200', //Size
-                r:'pg', //Rating
-                d:404 //Default
-            });
-            const newUser=new User({
-                name:req.body.name,
-                email:req.body.email,
-                avatar,
-                password:req.body.password
-            });
-            bcrypt.genSalt(10,(err,salt)=>{
-                bcrypt.hash(newUser.password,salt,(err,hash)=>{
-                    if(err) {
-                        throw err;
-                    }
-                    newUser.password=hash;
-                    newUser.save()
-                    .then(user=>res.json(user))
-                    .catch(err=>console.log(err));
-                })
-            })
-        }
-    })
-
-});
+  
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        errors.email = 'Email already exists';
+        return res.status(400).json(errors);
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', // Size
+          r: 'pg', // Rating
+          d: 'mm' // Default
+        });
+  
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+  
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  });
 
 
 //Login Routes
--- validation/login.js
+++ validation/login.js
@@ -4,8 +4,8 @@ const isEmpty= require('./is-empty');
 module.exports=function validateLoginInput(data){
     let errors={};
 
-    data.email=isEmpty(data.email)?data.email:'';
-    data.password=isEmpty(data.password)?data.password:'';
+    data.email=!isEmpty(data.email)?data.email:'';
+    data.password=!isEmpty(data.password)?data.password:'';
     
 
     if(!Validator.isEmail(data.email)){
