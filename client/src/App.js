0ad40f8d5732d8be244577b8ad0e810a05fce532
Karthik <karthik19894@gmail.com>
2018-07-08 00:12:47 +0530

    Started With Redux for State Management

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,30 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import './App.css';
+import {Provider} from 'react-redux';
+import store from './store';
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-        <Navbar/>
-        <Route exact path="/" component={Landing}/>
-        <div className="container">
-          <Route exact path="/login" component={Login}/>
-        </div>
-        <div className="container">
-          <Route exact path="/register" component={Register}/>
-        </div>
-        <Footer/>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+          <Navbar/>
+          <Route exact path="/" component={Landing}/>
+          <div className="container">
+            <Route exact path="/login" component={Login}/>
+          </div>
+          <div className="container">
+            <Route exact path="/register" component={Register}/>
+          </div>
+          <Footer/>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
