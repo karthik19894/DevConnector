ada2656e7524af846e09347edfad6c0d8102abd0
Karthik <karthik19894@gmail.com>
2018-07-23 00:06:58 +0530

    Started with Create Profile Form

--- client/src/App.js
+++ client/src/App.js
@@ -5,37 +5,36 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import CreateProfile from './components/create-profile/CreateProfile';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import {setCurrentUser, logoutUser} from './actions/authActions';
-import {clearCurrentProfile} from './actions/profileActions';
-
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
+import PrivateRoute from './components/common/PrivateRoute';
 
 //Check for token
 
-if(localStorage.jwtToken){
+if (localStorage.jwtToken) {
 	//Set auth token header
 	setAuthToken(localStorage.jwtToken);
 	//Decode token and get user info and expiry
-	const decoded=jwt_decode(localStorage.jwtToken);
+	const decoded = jwt_decode(localStorage.jwtToken);
 	//Set user and authenticated
 	store.dispatch(setCurrentUser(decoded));
 
 	//Check for expired token
-	const currentTime=Date.now()/1000;
-	if(decoded.exp<currentTime){
+	const currentTime = Date.now() / 1000;
+	if (decoded.exp < currentTime) {
 		store.dispatch(logoutUser());
 		//Clear Current Profile
 		store.dispatch(clearCurrentProfile());
 		//Redirect to Login
-		window.location.href='/login';
+		window.location.href = '/login';
 	}
-
-
 }
 
 class App extends Component {
@@ -48,12 +47,13 @@ class App extends Component {
 						<Route exact path="/" component={Landing} />
 						<div className="container">
 							<Route exact path="/login" component={Login} />
-						</div>
-						<div className="container">
 							<Route exact path="/register" component={Register} />
-						</div>
-						<div className="container">
-							<Route exact path="/dashboard" component={Dashboard} />
+							<Switch>
+								<PrivateRoute exact path="/dashboard" component={Dashboard} />
+							</Switch>
+							<Switch>
+								<PrivateRoute exact path="/create-profile" component={CreateProfile} />
+							</Switch>
 						</div>
 						<Footer />
 					</div>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
+import Spinner from '../common/Spinner';
+import { Link } from 'react-router-dom';
 
- class Dashboard extends Component {
-     componentDidMount(){
-         this.props.getCurrentProfile();
-     }
-  render() {
-    return (
-      <div>
-      <h1>Dashboard</h1>
-        
-      </div>
-    )
-  }
+class Dashboard extends Component {
+	componentDidMount() {
+		this.props.getCurrentProfile();
+	}
+	render() {
+		const { user } = this.props.auth;
+		const { profile, loading } = this.props.profile;
+
+		let dashboardContent;
+
+		if (profile === null || loading) {
+			dashboardContent = <Spinner />;
+		} else {
+			//Check if logged in user has profile data
+			if (Object.keys(profile).length > 0) {
+				dashboardContent = <h4>TODO: Display Profile</h4>;
+			} else {
+				dashboardContent = (
+					<div>
+						<p className="lead text-muted">Welcome {user.name} </p>
+						<p>You have not yet set up a profile, please create one</p>
+						<Link to="/create-profile" className="btn btn-lg btn-info">
+							Create Profile
+						</Link>
+					</div>
+				);
+			}
+		}
+
+		return (
+			<div className="dashboard">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<h1 className="display-4">Dashboard</h1>
+							{dashboardContent}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default connect(null,{getCurrentProfile})(Dashboard);
+Dashboard.propTypes = {
+	getCurrentProfile: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+	profile: state.profile,
+	auth: state.auth,
+});
+
+export default connect(
+	mapStateToProps,
+	{ getCurrentProfile }
+)(Dashboard);
