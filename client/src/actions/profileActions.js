72d4f7452a9372ed44c7ca8717e46ba0bff59d74
Karthik <karthik19894@gmail.com>
2018-07-23 22:59:09 +0530

    Create Profile Action Done

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,30 +1,45 @@
 import axios from 'axios';
-import { GET_PROFILE,PROFILE_LOADING,GET_ERRORS, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from './types';
 
 //Get current profile
-export const getCurrentProfile=()=>dispatch=>{
-    dispatch(setProfileLoading());
-    axios.get('/api/profile')
-    .then(res=>{
-        dispatch({
-            type:GET_PROFILE,
-            payload:res.data
-        })
-    })
-    .catch(err=>dispatch({
-        type:GET_PROFILE,
-        payload:{}
-    }))
-}
+export const getCurrentProfile = () => dispatch => {
+	dispatch(setProfileLoading());
+	axios
+		.get('/api/profile')
+		.then(res => {
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data,
+			});
+		})
+		.catch(err =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: {},
+			})
+		);
+};
 
-export const setProfileLoading=()=>dispatch=>{
-    dispatch({
-        type:CLEAR_CURRENT_PROFILE
-    })
-}
+export const setProfileLoading = () => dispatch => {
+	dispatch({
+		type: CLEAR_CURRENT_PROFILE,
+	});
+};
 
-export const clearCurrentProfile=()=>dispatch=>{
-    dispatch({
-        type:CLEAR_CURRENT_PROFILE
-    });
-}
+export const clearCurrentProfile = () => dispatch => {
+	dispatch({
+		type: CLEAR_CURRENT_PROFILE,
+	});
+};
+
+export const createProfile = (profileData, history) => dispatch => {
+	axios
+		.post('/api/profile', profileData)
+		.then(res => history.push('/dashboard'))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
+};
