9d1ac88a60990ce85125e53d456ca98bfeea08fd
Karthik <karthik19894@gmail.com>
2018-08-08 22:49:59 +0530

    Redux tools extension removed for production env

--- client/src/store.js
+++ client/src/store.js
@@ -6,12 +6,18 @@ const initialState = {};
 
 const middleware = [thunk];
 
+let devTools = null;
+
+if (process.env.NODE_ENV !== 'production') {
+	devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+}
+
 const store = createStore(
 	rootReducer,
 	initialState,
 	compose(
 		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		devTools
 	)
 );
 
